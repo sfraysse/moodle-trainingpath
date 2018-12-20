9ed6389889dd5d36b417c5fa4967d598b5a03454
Sébastien Fraysse <sebastien@fraysse.eu>
2018-12-20 14:01:38 +0100

    Solved scoring issue when leaving the SCORM content unproperly

---
+++ hooks.php
@@ -0,0 +1,33 @@
+<?php
+
+/* * *************************************************************
+ *  This script has been developed for Moodle - http://moodle.org/
+ *
+ *  You can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+  *
+ * ************************************************************* */
+
+require_once($CFG->dirroot . '/mod/trainingpath/report/lib.php');
+require_once($CFG->dirroot . '/mod/trainingpath/locallib.php');
+require_once($CFG->dirroot . '/mod/scormlite/report/reportlib.php');
+
+
+// Completion hook
+
+function trainingpath_hook_completion($cm, $learningpath, $sco, $userid, $attempt) {
+	global $DB;
+	$trackdata = scormlite_get_mystatus($cm, $sco, false, false)[1];
+	$item = $DB->get_record('trainingpath_item', array('ref_id' => $sco->id), '*', MUST_EXIST);
+	if (!$item->complementary) trainingpath_report_record_scormlite_track($trackdata, $item, $learningpath, $item->evaluation);
+}
